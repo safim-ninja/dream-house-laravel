d1227e4e721a624af11f6b8c3766b2e78513bf95
bristy-saha <bristy15-1077@diu.edu.bd>
2023-11-01 14:14:42 +0600

    authentication

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index')->name('home');
-});
+    return view('index');
+})->name('home');
 
 Route::get('/dashboard', function () {
     return view('index');
