2c7fbc5526890ef1308d603055dce29adbe7ad12
Safim Md Fahim <safim.me@gmail.com>
2023-11-09 16:18:49 +0600

    added fake users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,9 +19,17 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'dob' => fake()->date(),
+            'email' => fake()
+                ->unique()
+                ->safeEmail(),
+            'phone' => fake()->unique()->phoneNumber,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'photo' => fake()->imageUrl('128', '128'),
+            'role' => fake()->randomElement(['owner', 'user']),
+            'name' => fake()->name(),
+
             'remember_token' => Str::random(10),
         ];
     }
@@ -31,8 +39,10 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
+        return $this->state(
+            fn(array $attributes) => [
+                'email_verified_at' => null,
+            ],
+        );
     }
 }
