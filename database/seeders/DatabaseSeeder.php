6d14e0656311f9bf8949ee4b6644721c0fbf357e
Safim Md Fahim <safim.me@gmail.com>
2023-11-09 16:19:47 +0600

    added users users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
-
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,13 +14,47 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Admin
         DB::table('users')->insert([
-            'role' => 0,
             'name' => 'Admin',
-            'dob' => '2023-10-05',
+            'dob' => '2000-12-05',
             'email' => 'admin.dh@gmail.com',
             'phone' => '0123456789',
+            'email_verified_at' => now(),
             'password' => Hash::make('1234%^&*'),
+            'role' => 'admin',
         ]);
+        // Owners
+        DB::table('users')->insert([
+            'name' => 'House Owner',
+            'dob' => '2000-10-09',
+            'email' => 'owner@gmail.com',
+            'phone' => '1234567567',
+            'email_verified_at' => now(),
+            'password' => Hash::make('1234%^&*'),
+            'role' => 'owner',
+        ]);
+        DB::table('users')->insert([
+            'name' => 'House Owner 2',
+            'dob' => '2000-10-10',
+            'email' => 'owner2@gmail.com',
+            'phone' => '12345675213',
+            'email_verified_at' => now(),
+            'password' => Hash::make('1234%^&*'),
+            'role' => 'owner',
+            'verification' => 'verified',
+        ]);
+        // User
+        DB::table('users')->insert([
+            'name' => 'Normal User',
+            'dob' => '2001-01-13',
+            'email' => 'user@gmail.com',
+            'phone' => '1234323423',
+            'email_verified_at' => now(),
+            'password' => Hash::make('1234%^&*'),
+            'role' => 'user',
+        ]);
+
+        \App\Models\User::factory(10)->create();
     }
 }
